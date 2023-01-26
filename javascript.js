c4b7e215b87a2154413291499b9dcd2324cc2358
John Besco <johnbesco.dev@gmail.com>
2023-01-26 11:12:04 +0000

    Add blue text

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+// Add red text
+
 const container = document.querySelector('#container');
 
 const content = document.createElement('p');
@@ -8,3 +10,16 @@ content.classList.add('content');
 content.textContent = "Hey I'm red!";
 
 container.appendChild(content);
+
+// Add blue text
+
+const moreContent = document.createElement('h3');
+
+moreContent.style.color = 'blue';
+
+moreContent.classList.add('blueText');
+moreContent.textContent = 'Im a blue h3!';
+
+container.appendChild(moreContent)
+
+
