fe7489b5e0034678b577095f8c5ef52207e4615a
John Besco <johnbesco.dev@gmail.com>
2023-02-07 22:51:21 +0000

    JS Practice

--- javascript.js
+++ javascript.js
@@ -13,3 +13,30 @@ someRedText.textContent = 'Hey im red!'
 someRedText.style.color = 'red'
 
 container.appendChild(someRedText);
+
+const blueText = document.createElement('h3');
+blueText.classList.add('blueText');
+blueText.textContent = 'Im a blue h3!'
+
+blueText.style.color = 'blue';
+
+container.appendChild(blueText);
+
+const funkyDiv = document.createElement('div');
+funkyDiv.classList.add('funky');
+
+funkyDiv.style.border = '1px solid black';
+funkyDiv.style.backgroundColor = 'pink';
+
+const anotherH1 = document.createElement('h1');
+anotherH1.textContent = 'Im in a div';
+
+const someText = document.createElement('p');
+someText.textContent = 'ME TOO!';
+
+funkyDiv.appendChild(anotherH1);
+funkyDiv.appendChild(someText);
+
+
+container.appendChild(funkyDiv);
+
